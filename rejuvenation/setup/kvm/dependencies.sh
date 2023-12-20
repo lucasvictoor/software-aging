7630361dc524005dc80830924ea59148894f6012
ThaysonScript <thaysonguedes01@gmail.com>
2023-12-20 12:27:32 -0300

    install dependencies with libvirt

--- rejuvenation/setup/kvm/dependencies.sh
+++ rejuvenation/setup/kvm/dependencies.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-INSTALL_KVM_DEPENDENCIES() {
+INSTALL_KVM_LIBVIRT_DEPENDENCIES() {
+    reset
+
+    if which qemu-system-x86_64 >/dev/null; then
+        apt install qemu-system libvirt-daemon-system -y
+    fi
+
+    # add root user group on libvirt
+    adduser "$USER" libvirt
+}
+
+INSTALL_KVM_WITHOUT_LIBVIRT_DEPENDENCIES() {
     reset
 
     if ! which qemu-system-x86_64 > /dev/null; then
@@ -8,4 +19,13 @@ INSTALL_KVM_DEPENDENCIES() {
     fi
 }
 
-INSTALL_KVM_DEPENDENCIES
+read -rp "[1] - to download kvm with virtlib [2] - to download kvm without virtlib: " choice
+
+if [[ "$choice" -eq 1 ]]; then
+    INSTALL_KVM_LIBVIRT_DEPENDENCIES
+
+elif [[ "$choice" -eq 2 ]]; then
+    INSTALL_KVM_WITHOUT_LIBVIRT_DEPENDENCIES
+else
+    echo "enter valid value!"
+fi
