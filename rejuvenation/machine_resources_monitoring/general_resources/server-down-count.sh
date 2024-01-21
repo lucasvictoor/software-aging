ff3c57467a0fd563a8851e5589b5a9605285a8a2
ThaysonScript <thaysonguedes01@gmail.com>
2024-01-20 23:00:54 -0300

    organizations and kvm network settings

--- rejuvenation/machine_resources_monitoring/general_resources/server-down-count.sh
+++ rejuvenation/machine_resources_monitoring/general_resources/server-down-count.sh
@@ -3,16 +3,39 @@
 # Script to monitor the status of the server by sending HTTP requests to http://localhost:8080
 # and logging downtime information to a CSV file
 
+# ############################## IMPORTS #############################
+source ../../virtualizer_functions/kvm_functions.sh
+# ####################################################################
+
 started_at=0
 is_offline=0
 down_count=0
+
+VIRTUALIZER_TYPE=$1
+
+if [[ "$VIRTUALIZER_TYPE" == "vbox" ]]; then
+  # url="192.168.122.114"
+  url="http://localhost:8080"
+
+elif [[ "$VIRTUALIZER_TYPE" == "kvm" ]]; then
+  # url="192.168.122.114"
+  url="$GET_HOST_IP:8080"
+
+elif [[ "$VIRTUALIZER_TYPE" == "xen" ]]; then
+  echo -e "nada por enquanto"
+  exit 1
+
+else
+  echo -e "erro ao tentar executar ./server-down-count.sh em obter ip do server"
+  exit 1
+fi
 
 while true; do
   if [ "$is_offline" -eq 0 ]; then
     started_at=$(date +%s)
   fi
 
-  response=$(curl -s -o /dev/null -w "%{http_code}"  -m 5  http://localhost:8080)
+  response=$(curl -s -o /dev/null -w "%{http_code}" -m 5 "$url")
 
   if [ "$response" -eq 200 ]; then
     if [ "$is_offline" -eq 1 ]; then
---
+++ rejuvenation/setup/kvm/create_network_redirect_settings.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# ############################## IMPORTS #############################
+source ../../virtualizer_functions/kvm_functions.sh
+# ####################################################################
+
+VM_NAME=$1
+
+readonly GUEST_IP="$GET_GUEST_IP"
+readonly HOST_IP="$GET_HOST_IP"
+
+readonly NETWORK_INTERFACE="virbr0"
+
+readonly REDIRECT_SSH_PORT="2222, 22"       # origem, destino
+readonly REDIRECT_NGINX_PORT="8080, 80"     # origem, destino
+
+echo -e "{
+    // Note: comments in two styles are supported
+    /* Note: comments in two styles are supported */
+
+    // Name of the guest VM
+    \"$VM_NAME\": {
+
+        // Name of the network interface the guest is using
+        \"interface\": \"$NETWORK_INTERFACE\",
+
+        // IP address of the guest
+        \"private_ip\": \"$GUEST_IP\",
+
+        // Remote IP which gets permission to access the ports
+        // This line can be omitted, allowing any remote IP access
+       // \"source_ip\": \"$HOST_IP\",
+
+        /*
+        When opening ports, you have two choices:
+        1. Opening single ports one by one using \"port_map\"
+           This allows you to map an external port x to an internal port y if you wish
+
+        2. Opening a range of ports altogether using \"port_range\"
+
+        You can use one or both of these, as the examples below illustrate.
+        */
+
+        \"port_map\": {
+            // Protocol can be one of tcp, udp or icmp
+            \"tcp\": [
+                [$REDIRECT_SSH_PORT],   // ssh redirect port
+                [$REDIRECT_NGINX_PORT]  // nginx redirect port
+            ]
+        }
+    }
+}" > /etc/libvirt/hooks/hooks.json
+
+[[ $? -eq 0 ]] && printf "\nredirecionamento de rede configurado\n" || printf "\nerro em configuracoes de redirecionamento de rede\n"
