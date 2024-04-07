6effcdc8894671fc8ce805eb08d9060f70e5cbd6
Lucas Victor <lv8790121@gmail.com>
2024-04-07 07:31:50 -0300

    Workload LXC

---
+++ rejuvenation/workloads/lxc_workload.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+######################################## LXC - WORKLOAD #######################################
+# ABOUT:                                                                                      #
+#   used to simulate workload on (LXC) virtualization infrastructure                          #
+#                                                                                             #
+# WORKLOAD TYPE:                                                                              #
+#   DISKS                                                                                     #
+###############################################################################################
+
+# ####################### IMPORTS #######################
+source ./virtualizer_functions/lxc_functions.sh
+# #######################################################
+
+readonly wait_time_after_attach=10
+readonly wait_time_after_detach=10
+
+LXC_WORKLOAD() {
+  local count_disks=1
+  local max_disks=50
+  #local disk_path="path/to/your/lxc/disks/disk"
+
+  while true; do
+    # attach
+    for port in {1..3}; do
+      ATTACH_DISK "${disk_path}${count_disks}.img" "$port"
+
+      if [[ "$count_disks" -eq "$max_disks" ]]; then
+        count_disks=1
+      else
+        ((count_disks++))
+      fi
+      sleep $wait_time_after_attach
+    done
+
+    # detach
+    for port in {1..3}; do
+      DETACH_DISK "$port"
+      sleep $wait_time_after_detach
+    done
+  done
+}
+
+LXC_WORKLOAD
