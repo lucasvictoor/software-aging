55977f285cf48deeef6f8bf604ee6cb2a2d2d091
Lucas Victor <71571447+lucasvictoor@users.noreply.github.com>
2024-04-09 15:15:59 -0300

    Updated setupVm.sh

--- rejuvenation/setup/lxc/setupVm.sh
+++ rejuvenation/setup/lxc/setupVm.sh
@@ -6,18 +6,28 @@ source ../../virtualizer_functions/lxc_functions.sh
 
 readonly VM_NAME="vmDebian"
 
+# FUNCTION=CREATE_VM()
+# DESCRIPTION:
+# 
+# init - initialize a new VM debian12
+# override - sets the disk size
+# limits.cpu and limits.memory - configure cpu and memory limit
+# disk source - adds and installation disk to the VM
+# install openssh-server - installation ssh
+# installl nginx - installation web server
 CREATE_VM() {
   local rootfs="/var/lib/lxc/$VM_NAME/rootfs"
 
-  case $DISTRO_ID in
-  "debian" | "ubuntu")
-    lxc-create -n "$VM_NAME" -t download -- -d $DISTRO_ID -r focal -a amd64
-    ;;
-  *)
-    echo "Distribuição não suportada para criação de VM LXC."
-    exit 1
-    ;;
-  esac
+  lxc init debian12 --vm --empty
+  lxc config device override debian12 root size=5G
+  lxc config set debian12 limits.cpu=2 limits.memory=512M
+  #lxc config device add debian12 vtpm tpm path=/dev/tpm0
+  
+  lxc exec debian12 -- apt-get update
+  lxc exec debian12 -- apt-get install -y openssh-server
+  lxc exec debian12 -- apt-get install -y nginx
+  
+  lxc config device add install disk source=/path/to/software-aging/debian12.lxd.iso boot.priority=10
 }
 
 START_VM() {
